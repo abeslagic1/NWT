bdc74a0943e1ac7fe52ee0f062fe6240c31f1f8d
amar.beslagic <amar.beslagic@bstelecom.ba>
2024-04-16 15:50:29 +0200

    Dodane putanje za update i delete sobe.

--- registration-service/src/main/java/com/etf/controller/SobaController.java
+++ registration-service/src/main/java/com/etf/controller/SobaController.java
@@ -2,6 +2,7 @@ package com.etf.controller;
 
 import com.etf.dao.SobaDAO;
 import com.etf.exceptions.NotFoundException;
+import com.etf.model.Krevet;
 import com.etf.model.Soba;
 import com.etf.repository.SobaRepository;
 import jakarta.validation.Valid;
@@ -72,6 +73,99 @@ public class SobaController {
 
             return ResponseEntity.ok(soba);
         }catch (NotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping(path = "/Naziv/{id}")
+    public @ResponseBody ResponseEntity<String> updateSobaNaziv(@RequestBody String naziv, @PathVariable("id") Integer id) {
+
+        try {
+            Optional<Soba> soba = sobaRepository.findById(id);
+
+            if (soba.isEmpty()) throw new NotFoundException("Soba sa tim id-em nije pronadjen.");
+            if (naziv == null || naziv.isEmpty() || naziv.isBlank())
+                throw new NotFoundException("Naziv ne smije biti prazan string.");
+
+            Soba soba1 = soba.get();
+
+            soba1.setNazivSobe(naziv);
+
+            sobaRepository.save(soba1);
+
+            return ResponseEntity.ok("The room has been successfully updated");
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PatchMapping(path = "/zauzetost/{id}")
+    public @ResponseBody ResponseEntity<String> updateSobaZauzetost(@RequestBody String zauzetost, @PathVariable("id") Integer id){
+
+        try{
+            Optional<Soba> soba = sobaRepository.findById(id);
+
+            if(soba.isEmpty()) throw new NotFoundException("Soba sa tim id-em nije pronadjen.");
+            if(zauzetost == null || zauzetost.isEmpty() || zauzetost.isBlank()) throw new NotFoundException("Polje zauzetost ne smije biti prazan string.");
+
+            Soba soba1 = soba.get();
+
+            if(zauzetost.equals("true") || zauzetost.equals("false")){
+                Boolean zauzet = Boolean.valueOf(zauzetost);
+
+                soba1.setZauzetost(zauzet);
+                sobaRepository.save(soba1);
+
+                return ResponseEntity.ok("The room has been successfully updated");
+            }
+            return new ResponseEntity<>("Nije ispravna vrijednost boolean-a.", HttpStatus.BAD_REQUEST);
+        }
+        catch (NotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PatchMapping(path = "/privateShared/{id}")
+    public @ResponseBody ResponseEntity<String> updateSobaPrivateShared(@RequestBody String privateShared, @PathVariable("id") Integer id){
+
+        try{
+            Optional<Soba> soba = sobaRepository.findById(id);
+
+            if(soba.isEmpty()) throw new NotFoundException("Soba sa tim id-em nije pronadjen.");
+            if(privateShared == null || privateShared.isEmpty() || privateShared.isBlank()) throw new NotFoundException("Private/Shared ne smije biti prazan string.");
+
+            Soba soba1 = soba.get();
+
+            if(privateShared.equals("S") || privateShared.equals("P")){
+                soba1.setPrivateShared(privateShared);
+
+                sobaRepository.save(soba1);
+
+                return ResponseEntity.ok("The room has been successfully updated");
+            }
+
+            return new ResponseEntity<>("Nije unesena ispravna vrijednost za polje Private/Shared. Unesite P ili S.", HttpStatus.BAD_REQUEST);
+        }
+        catch (NotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(path = "/byId/{id}")
+    public @ResponseBody ResponseEntity<String> deleteSobaById(@PathVariable("id") Integer id){
+
+        try{
+            Optional<Soba> soba = sobaRepository.findById(id);
+
+            if(soba.isEmpty()) throw new NotFoundException("Soba sa tim id-em nije pronadjen.");
+
+            sobaRepository.deleteById(id);
+
+            return ResponseEntity.ok("The room has been successfully deleted");
+        }
+        catch (NotFoundException e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
